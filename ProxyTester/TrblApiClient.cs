6c92336283fa4deb88156f2b891b80200558dfed
trbl <trbl@her.st>
2021-03-21 13:03:21 +0100

    threaded tester

--- ProxyTester/TrblApiClient.cs
+++ ProxyTester/TrblApiClient.cs
@@ -24,7 +24,7 @@ namespace ProxyTester
         public async Task<bool> Login(string user, string pass)
         {
             var creds = new UserInfo { Username = "trbl", Password = "herst" };
-            var content = new StringContent(JsonSerializer.Serialize(creds), Encoding.UTF8, "application/json");
+            using var content = new StringContent(JsonSerializer.Serialize(creds), Encoding.UTF8, "application/json");
             var response = await httpClient.PostAsync($"{ENDPOINT}/Token", content);
             var token = await response.Content.ReadAsStringAsync();
 
@@ -49,13 +49,13 @@ namespace ProxyTester
         }
         public async Task<bool> UpdateProxy(Proxy proxy)
         {
-            var content = new StringContent(JsonSerializer.Serialize(proxy), Encoding.UTF8, "application/json");
+            using var content = new StringContent(JsonSerializer.Serialize(proxy), Encoding.UTF8, "application/json");
             var putResponse = await httpClient.PatchAsync($"{ENDPOINT}/proxy", content);
             return putResponse.StatusCode == HttpStatusCode.OK;
         }
         public async Task<bool> DeleteProxy(Proxy proxy)
         {
-            var response = await httpClient.DeleteAsync("http://127.0.0.1:5000/api/proxy?id=" + proxy.Id);
+            var response = await httpClient.DeleteAsync($"{ENDPOINT}/proxy?id=" + proxy.Id);
             return response.StatusCode == HttpStatusCode.OK;
         }
     }
